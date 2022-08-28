96d922a79fbca80769d0c96cbad8760ad7590915
ckartik <k5chopra@uwaterloo.ca>
2022-08-27 21:58:09 -0400

    Adds all of clone.

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "upload.wikimedia.org",
+      "cdn.freelogovectors.net",
+      "a0.muscache.com",
+      "images.contentstack.io",
+      "pbs.twimg.com",
+      "links.papareact.com"
+    ],
+  },
+};
